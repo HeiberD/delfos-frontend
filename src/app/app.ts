809ad003750e2f17a269da895528d881b8230b52
HeiberD <heiberdiazp@gmail.com>
2026-01-08 20:00:49 -0500

    features: Define componentes de pagina, shared: Define los componentes de layout, app.routes.ts: Define las rutas base de la aplicacion

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./shared/layout/header/header";
+import { Footer } from "./shared/layout/footer/footer";
+
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'app-root',  
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  imports: [RouterOutlet, Header, Footer]
 })
 export class App {
   protected readonly title = signal('frontend');
