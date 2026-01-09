193a7541e7ffae74a48d6ce71edc7d742ceffff8
Nilsson-AvH <nlopez153@misena.edu.co>
2026-01-08 22:03:29 -0500

    [+] header.ts: Define el menu principalen el header [+]app.routes.ts: Define las rutas para los componentes de producto [+] user.(list,new.form.edit-form): crea componentes base

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,26 @@ import { Login } from './features/pages/login/login';
 import { Register } from './features/pages/register/register';
 import { PageNotFound } from './features/pages/page-not-found/page-not-found';
 import { Home } from './features/pages/home/home';
+import { UserList } from './features/pages/users/user-list/user-list';
+import { UserNewForm } from './features/pages/users/user-new-form/user-new-form';
+import { UserEditForm } from './features/pages/users/user-edit-form/user-edit-form';
 
 export const routes: Routes = [
+
+    // Root routes
     { path: 'home', component: Home},
+    { path: '404', component: PageNotFound },
+    
+    // Auth routes
     { path: 'login', component: Login},
     { path: 'register', component: Register},
-    { path: '404', component: PageNotFound },
+
+    // User routes
+    { path: 'dashboard/users', component: UserList},
+    { path: 'dashboard/user/new', component: UserNewForm},
+    { path: 'dashboard/user/edit', component: UserEditForm},
+    
+    // Default routes
     { path: '', redirectTo: 'home', pathMatch: 'full'},
     { path: '**', redirectTo: '404', pathMatch: 'full'}    
 ];
