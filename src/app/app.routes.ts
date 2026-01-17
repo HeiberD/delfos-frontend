2e3e2331dd51e653cf2e4b2dc55c100f9ae27603
Nilsson-AvH <nlopez153@misena.edu.co>
2026-01-16 21:20:45 -0500

    creacion de footer y app.routes, header.html, header.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,17 @@ import { Login } from './features/pages/login/login';
 import { Register } from './features/pages/register/register';
 import { PageNotFound } from './features/pages/page-not-found/page-not-found';
 import { Home } from './features/pages/home/home';
+import { UsersList } from './features/pages/users/users-list/users-list';
+import { AdministrativeUserNewForm } from './features/pages/users/administrative-user-new-form/administrative-user-new-form';
+import { UserEditForm } from './features/pages/users/user-edit-form/user-edit-form';
 
 export const routes: Routes = [
     { path: 'home', component: Home},
     { path: 'login', component: Login},
     { path: 'register', component: Register},
+    { path: 'dashboard/users', component: UsersList},
+    { path: 'dashboard/administrative-user/new', component: AdministrativeUserNewForm},
+    { path: 'dashboard/user/edit/:id', component: UserEditForm},
     { path: '404', component: PageNotFound },
     { path: '', redirectTo: 'home', pathMatch: 'full'},
     { path: '**', redirectTo: '404', pathMatch: 'full'}    
---
+++ src/app/features/pages/users/administrative-user-new-form/administrative-user-new-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-administrative-user-new-form',
+  imports: [],
+  templateUrl: './administrative-user-new-form.html',
+  styleUrl: './administrative-user-new-form.css',
+})
+export class AdministrativeUserNewForm {
+
+}
