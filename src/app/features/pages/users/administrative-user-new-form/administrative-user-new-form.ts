0ea3ba025d941a57048ffb65c68e6dba290ba65c
Nilsson-AvH <nlopez153@misena.edu.co>
2026-01-16 22:44:43 -0500

    [+] administrative-user-new-form deifne la estructura del formulario y establece 'binding' comunicacion entre el modelo y la vista del componente

--- src/app/features/pages/users/administrative-user-new-form/administrative-user-new-form.ts
+++ src/app/features/pages/users/administrative-user-new-form/administrative-user-new-form.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-administrative-user-new-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './administrative-user-new-form.html',
   styleUrl: './administrative-user-new-form.css',
 })
 export class AdministrativeUserNewForm {
+  formData!: FormGroup;
 
+  constructor() {
+    this.formData = new FormGroup({
+      role: new FormControl('auditor'),
+      nuip: new FormControl(''),
+      names: new FormControl(''),
+      lastName: new FormControl(''),
+      secondLastName: new FormControl (''),
+      jobTitle: new FormControl(''),
+      email: new FormControl(''),
+      password: new FormControl(''),
+      status: new FormControl('inactive')
+    })
+  }
 }
